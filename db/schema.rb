98710f4d01046e1813f1569ca743d5f2c730984c
Xiaoxuan Wang <wangxiaoxuan4869@gmail.com>
2020-05-17 15:42:20 -0700

    Add Message resource

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_17_213908) do
+ActiveRecord::Schema.define(version: 2020_05_17_222633) do
+
+  create_table "messages", force: :cascade do |t|
+    t.text "body"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
