20aafeb9da0bc6d12e25deed1b2d9e79c2b0ef38
Xiaoxuan Wang <wangxiaoxuan4869@gmail.com>
2020-05-18 15:08:15 -0700

    Add auth methods and use form_for for login form

--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -3,6 +3,5 @@ class ChatroomController < ApplicationController
     # @messages = Message.find_by_sql("SELECT username, body FROM users
     # RIGHT OUTER JOIN messages ON users.id = messages.user_id")
     @messages = Message.all
-    p @messages
   end
 end
