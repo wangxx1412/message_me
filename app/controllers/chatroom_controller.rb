2e3d3187b77dc0d9ac8c8751e38ccf55fbc7a78f
Xiaoxuan Wang <wangxiaoxuan4869@gmail.com>
2020-05-21 12:12:54 -0700

    Update messages scope display and hide submit button

--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -3,8 +3,8 @@ class ChatroomController < ApplicationController
   def index
     # @messages = Message.find_by_sql("SELECT username, body FROM users
     # RIGHT OUTER JOIN messages ON users.id = messages.user_id")
-    @messages = Message.all
-
+    # @messages = Message.all
+    @messages = Message.custom_display
     @message = Message.new
   end
 end
